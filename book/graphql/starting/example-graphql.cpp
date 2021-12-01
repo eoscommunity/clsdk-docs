fe302aa37fa57f35f6f7d9685957f85fbf1a46a8
Todd Fleming <tbfleming@gmail.com>
2021-12-01 16:43:25 -0500

    update

--- book/graphql/starting/example-graphql.cpp
+++ book/graphql/starting/example-graphql.cpp
@@ -1,35 +1,24 @@
-#include <btb/graphql.hpp>
-#include <eosio/reflection2.hpp>
 #include "example.hpp"
 
-// GraphQL proxy for example::animal
-struct Animal
-{
-   const example::animal* obj;
+// GraphQL Support
+#include <btb/graphql.hpp>
 
-   // These methods have no arguments, so act like fields in GraphQL
-   auto name() const { return obj->name; }
-   auto type() const { return obj->type; }
-   auto owner() const { return obj->owner; }
-   auto purchasePrice() const { return obj->purchase_price; }
+// EOSIO_REFLECT2; augments methods with argument names
+#include <eosio/reflection2.hpp>
 
-   // This method has an argument, so needs method(...) in the
-   // EOSIO_REFLECT2 definition below.
-   auto isA(eosio::name type) const { return type == obj->type; }
-};
-EOSIO_REFLECT2(Animal, name, type, owner, purchasePrice, method(isA, "type"))
-
+// The root of GraphQL queries
 struct Query
 {
+   // Identify the contract
    eosio::name contract;
 
-   // Returns a Proxy object instead of returning the original object
-   std::optional<Animal> animal(eosio::name name) const
+   // Retrieve an animal, if it exists
+   std::optional<example::animal> animal(eosio::name name) const
    {
       example::animal_table table{contract, contract.value};
       auto it = table.find(name.value);
       if (it != table.end())
-         return Animal{&*it};
+         return *it;
       else
          return std::nullopt;
    }
@@ -39,12 +28,14 @@ EOSIO_REFLECT2(Query,                  //
                method(animal, "name")  // query a method; identifies the argument names
 )
 
+// Action: execute a GraphQL query and print the result
 void example::example_contract::graphql(const std::string& query)
 {
    Query root{get_self()};
    eosio::print(btb::gql_query(root, query, ""));
 }
 
+// Action: print the GraphQL schema
 void example::example_contract::graphqlschema()
 {
    eosio::print(btb::get_gql_schema<Query>());
